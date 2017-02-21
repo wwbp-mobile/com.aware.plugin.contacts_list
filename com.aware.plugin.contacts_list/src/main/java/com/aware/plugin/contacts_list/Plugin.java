1dca2f9c6dbca1a36aa1a1bce0dfeeaf36f9bea6
Denzil Ferreira <denzil.ferreira@ee.oulu.fi>
2017-02-21 16:08:40 +0200

    - Don't override main resources from library

--- com.aware.plugin.contacts_list/src/main/java/com/aware/plugin/contacts_list/Plugin.java
+++ com.aware.plugin.contacts_list/src/main/java/com/aware/plugin/contacts_list/Plugin.java
@@ -79,6 +79,8 @@ public class Plugin extends Aware_Plugin {
                 e.printStackTrace();
             }
 
+            Aware.startAWARE(this);
+
         } else {
             Intent permissions = new Intent(this, PermissionsHandler.class);
             permissions.putExtra(PermissionsHandler.EXTRA_REQUIRED_PERMISSIONS, REQUIRED_PERMISSIONS);
