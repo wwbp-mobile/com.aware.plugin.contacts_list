f0a15c877167cc9d96a26a0d47b21761cc7d2b0e
denzilferreira <ferreiradenzil@gmail.com>
2018-01-01 23:49:16 +0200

    - Code clean-up and robustness.

--- com.aware.plugin.contacts_list/src/main/java/com/aware/plugin/contacts_list/Plugin.java
+++ com.aware.plugin.contacts_list/src/main/java/com/aware/plugin/contacts_list/Plugin.java
@@ -2,19 +2,30 @@ package com.aware.plugin.contacts_list;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
 import com.aware.utils.Aware_Plugin;
+import com.aware.utils.Encrypter;
 import com.aware.utils.Scheduler;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Plugin extends Aware_Plugin {
 
     public static final String SCHEDULER_PLUGIN_CONTACTS = "SCHEDULER_PLUGIN_CONTACTS";
+
+    public static final String ACTION_REFRESH_CONTACTS = "ACTION_REFRESH_CONTACTS";
 
     @Override
     public void onCreate() {
@@ -43,6 +54,10 @@ public class Plugin extends Aware_Plugin {
 
         if (PERMISSIONS_OK) {
 
+            if (intent != null && intent.getAction() != null && intent.getAction().equalsIgnoreCase(ACTION_REFRESH_CONTACTS)) {
+                new AsyncContacts().execute(getApplicationContext());
+            }
+
             //Check if the user has toggled the debug messages
             DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
 
@@ -60,7 +75,7 @@ public class Plugin extends Aware_Plugin {
                     contacts_sync = new Scheduler.Schedule(SCHEDULER_PLUGIN_CONTACTS);
                     contacts_sync.setInterval(Integer.parseInt(Aware.getSetting(this, Settings.FREQUENCY_PLUGIN_CONTACTS))*60*24);//*60 mins/hrs * 24 hrs/day
                     contacts_sync.setActionType(Scheduler.ACTION_TYPE_SERVICE);
-                    contacts_sync.setActionClass(getPackageName() + "/" + Contacts_Service.class.getName());
+                    contacts_sync.setActionClass(getPackageName() + "/" + Plugin.class.getName());
                     Scheduler.saveSchedule(this, contacts_sync);
                 }
             }
@@ -83,6 +98,98 @@ public class Plugin extends Aware_Plugin {
         }
 
         return START_STICKY;
+    }
+
+    private static class AsyncContacts extends AsyncTask<Context, Void, Void> {
+        @Override
+        protected Void doInBackground(Context... contexts) {
+
+            Context context = contexts[0];
+
+            long sync_date = System.currentTimeMillis();
+
+            Cursor contacts = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            if (contacts != null && contacts.moveToFirst()) {
+                do {
+                    String contact_id = contacts.getString(contacts.getColumnIndex(ContactsContract.Contacts._ID));
+                    String contact_name = contacts.getString(contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    if (contact_name == null || contact_name.length() == 0) continue;
+
+                    JSONArray phone_numbers = new JSONArray();
+                    if (contacts.getInt(contacts.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) != 0) {
+                        Cursor phone = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contact_id, null, null);
+                        if (phone != null && phone.moveToFirst()) {
+                            do {
+                                try {
+                                    JSONObject phoneRow = new JSONObject();
+                                    phoneRow.put("type", phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE)));
+                                    phoneRow.put("number", phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                                    phoneRow.put("hash", Encrypter.hashPhone(context, phoneRow.getString("number")));
+                                    phone_numbers.put(phoneRow);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } while (phone.moveToNext());
+                        }
+                        if (phone != null && !phone.isClosed()) phone.close();
+                    }
+
+                    JSONArray emails = new JSONArray();
+                    Cursor email = context.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=" + contact_id, null, null);
+                    if (email != null && email.moveToFirst()) {
+                        do {
+                            try {
+                                JSONObject emailRow = new JSONObject();
+                                emailRow.put("type", email.getString(email.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE)));
+                                emailRow.put("email", email.getString(email.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)));
+                                emails.put(emailRow);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } while (email.moveToNext());
+                    }
+                    if (email != null && !email.isClosed()) email.close();
+
+                    JSONArray groups = new JSONArray();
+                    Cursor group = context.getContentResolver().query(
+                            ContactsContract.Data.CONTENT_URI, null,
+                            ContactsContract.Data.CONTACT_ID + "=" + contact_id + " AND " + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE + "'",
+                            null, null);
+                    if (group != null && group.moveToFirst()) {
+                        do {
+                            try {
+                                JSONObject groupRow = new JSONObject();
+                                groupRow.put("group", group.getString(group.getColumnIndex(ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID)));
+                                groups.put(groupRow);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } while (group.moveToNext());
+                    }
+                    if (group != null && !group.isClosed()) group.close();
+
+                    ContentValues contactInfo = new ContentValues();
+                    contactInfo.put(Provider.Contacts_Data.TIMESTAMP, System.currentTimeMillis());
+                    contactInfo.put(Provider.Contacts_Data.DEVICE_ID, Aware.getSetting(context, Aware_Preferences.DEVICE_ID));
+                    contactInfo.put(Provider.Contacts_Data.NAME, contact_name);
+                    contactInfo.put(Provider.Contacts_Data.PHONE_NUMBERS, phone_numbers.toString());
+                    contactInfo.put(Provider.Contacts_Data.EMAILS, emails.toString());
+                    contactInfo.put(Provider.Contacts_Data.GROUPS, groups.toString());
+                    contactInfo.put(Provider.Contacts_Data.SYNC_DATE, sync_date);
+
+                    try {
+                        context.getContentResolver().insert(Provider.Contacts_Data.CONTENT_URI, contactInfo);
+                        if (Aware.DEBUG) Log.d(Aware.TAG, "Contact stored: " + contactInfo.toString());
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    }
+                } while (contacts.moveToNext());
+            }
+            if (contacts != null && !contacts.isClosed()) contacts.close();
+
+            return null;
+        }
     }
 
     @Override
